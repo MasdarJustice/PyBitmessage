53e7f6f4e2711bb8fd5c929542e2c3ab5047b053
bikash617 <bikash.mnr@gmail.com>
2013-11-30 22:49:12 +0400

    added a functionality which adds the selected subscription to address book.

--- src/bitmessageqt/__init__.py
+++ src/bitmessageqt/__init__.py
@@ -2551,6 +2551,11 @@ class MyForm(QtGui.QMainWindow):
         currentInboxRow = self.ui.tableWidgetInbox.currentRow()
         toAddressAtCurrentInboxRow = str(self.ui.tableWidgetInbox.item(
             currentInboxRow, 0).data(Qt.UserRole).toPyObject())
+        
+         # CHANGE DONE BY BIKASH JOSHI (FEATURE-2)   
+        toAddressAtCurrentInboxRow_label = str(self.ui.tableWidgetInbox.item(
+            currentInboxRow, 0).text())
+        # END OF CHANGE
         fromAddressAtCurrentInboxRow = str(self.ui.tableWidgetInbox.item(
             currentInboxRow, 1).data(Qt.UserRole).toPyObject())
         msgid = str(self.ui.tableWidgetInbox.item(
@@ -2581,8 +2586,27 @@ class MyForm(QtGui.QMainWindow):
             if shared.safeConfigGetBoolean(toAddressAtCurrentInboxRow, 'chan'):
                 print 'original sent to a chan. Setting the to address in the reply to the chan address.'
                 self.ui.lineEditTo.setText(str(toAddressAtCurrentInboxRow))
-
-        self.ui.comboBoxSendFrom.setCurrentIndex(0)
+        
+         # CHANGE DONE BY BIKASH JOSHI (FEATURE-2)   
+         
+        AllItems = [str(self.ui.comboBoxSendFrom.itemText(i)) for i in range(self.ui.comboBoxSendFrom.count())]
+        AllItems_address = [str(self.ui.comboBoxSendFrom.itemData(i).toPyObject()) for i in range(self.ui.comboBoxSendFrom.count())]
+        
+        isPresent_Label = False
+        for item in AllItems:
+            if toAddressAtCurrentInboxRow_label == item:
+                isPresent_Label = True
+                break;
+        if(isPresent_Label == True):
+            currentIndex = AllItems.index(toAddressAtCurrentInboxRow_label)
+            self.ui.comboBoxSendFrom.setCurrentIndex(currentIndex)
+            
+        else:
+            currentIndex = AllItems_address.index(toAddressAtCurrentInboxRow)
+            self.ui.comboBoxSendFrom.setCurrentIndex(currentIndex)
+            
+         #END OF CHANGE   
+        #self.ui.comboBoxSendFrom.setCurrentIndex(0)
         self.ui.textEditMessage.setText('\n\n------------------------------------------------------\n' + unicode(messageAtCurrentInboxRow, 'utf-8)'))
         if self.ui.tableWidgetInbox.item(currentInboxRow, 2).text()[0:3] in ['Re:', 'RE:']:
             self.ui.lineEditSubject.setText(
